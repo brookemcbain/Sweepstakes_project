2cc49c440fff9746447e92af3d779c59934f9312
Brooke McBain <mcbainbrooke@gmail.com>
2020-10-06 15:33:05 -0500

    created contestant, sweepstakes interface, userinterface, and sweepstakes classes

---
+++ Sweepstakes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sweepstakes
+{
+    class Sweepstakes
+    {
+        public string name;
+        Random random; 
+        public Sweepstakes(string name)
+        {
+            this.name = name; 
+        }
+        public void RegisterContestant(Contestant contestant)
+        {
+            
+        }
+        public Contestant PickWinner()
+        {
+            
+        }
+        public void PrintContestantInfo(Contestant contestant)
+        {
+            UserInterface.DisplayContestantInfo(contestant); 
+
+        }
+    }
+}
---
+++ UserInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sweepstakes
+{
+    static class UserInterface
+    {
+        public static string GetUserInputString(string message)
+        {
+            Console.WriteLine(message);
+            string answer = Console.ReadLine();
+            return answer; 
+        }
+        
+    }
+}
